ea9bdf0aed4fb7ed31b673f3a9b4239502cd536d
Jose Fuentes <jose.fuentes.0107@gmail.com>
2024-03-14 18:39:39 -0500

    https fixed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let result =  document.getElementById("result");
 // Fetch Data from API 
 let getMovie = () => {
     let movieName = movieNameRef.value; 
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`
     //If input field is empty
     if(movieName.length <= 0){ 
         result.innerHTML =  `<h3 class="msg"> Please Enter a Movie Name </h3>`; 
